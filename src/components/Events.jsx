d57ced52a959fe191897cb5f354866dd15fad1b3
AntoAnson7 <antoanson6@gmail.com>
2023-05-18 22:02:18 +0530

    2

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -3,7 +3,17 @@ import "../styles/events.css"
 
 function Events() {
   return (
-    <div className='events'>Events</div>
+    <div className='events'>
+      <div>
+        <p>Your events ,</p>
+      </div>
+      <div className='events-section'>
+        EVENTS
+      </div>
+      <div>
+        <p>Recent Activity ,</p>
+      </div>
+    </div>
   )
 }
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNXdV4c05iAv4LAAUZF-4Zb8iG0XoCbVk",
@@ -10,3 +13,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
+
+export const storage = getStorage(app);
